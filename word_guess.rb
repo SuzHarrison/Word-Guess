ac9efc06af5fb2ce020b25a33a2f0cc719e37e3c
Jillian Boshart <Jilliliz@gmail.com>
2016-02-25 14:43:32 -0800

    Added welcome message, started the program. Our current issue is initializing the class correctly (code is currently broken).

--- word_guess.rb
+++ word_guess.rb
@@ -1,8 +1,7 @@
 class WordGuess
-  def initialize answer
-    @word = ["green", "purple", "yellow", "pink"].sample
+  def initialize(new_game)
+    @word = new_game["green", "purple", "yellow", "pink"].sample
   end
-
 
   def correct
     if guess == true
@@ -10,18 +9,40 @@ class WordGuess
 
       else
         puts "You win!" #answer!!
-
-
-
-
-
-
-
-      " "#x= a correct letter of the answer
-      puts "Good job!"
-      puts "Guess again!"
-      guess = gets.chomp
-    else
-      puts "Bad news! Bunnies are multiplying! Please guess again."
-      guess = gets.chomp
+      end
     end
+  end
+end
+
+# Start of program:
+welcome = <<YES
+
+Hello, home owner! We're going to play a game. I'm going to make you guess a
+word, and for each time you guess a letter incorrectly a new BUNNY is going to
+start to dig up your yard! HAHAHAHAHA!
+
+Are you ready? Take a guess!
+
+YES
+
+new_game = WordGuess.new
+
+puts welcome
+guess = gets.chomp
+
+letter_array = @word.split(//)
+puts letter_array
+
+
+
+
+
+#      " "#x= a correct letter of the answer
+#      puts "Good job!"
+#      puts "Guess again!"
+#      guess = gets.chomp
+#    else
+#      puts "Bad news! Bunnies are multiplying! Please guess again."
+#      guess = gets.chomp
+#    end
+#end
