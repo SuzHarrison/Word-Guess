9aabad18579d8dfffacaff0d0189fc4df4bdba47
Suzanne Harrison <harrison_family@hotmail.com>
2016-02-25 14:25:43 -0800

    word_guess.rb

--- word_guess.rb
+++ word_guess.rb
@@ -1,1 +1,27 @@
-class WordGuess 
+class WordGuess
+  def initialize answer
+    @word = ["green", "purple", "yellow", "pink"].sample
+  end
+
+
+  def correct
+    if guess == true
+      if puts ## add one letter of the answer
+
+      else
+        puts "You win!" #answer!!
+
+
+
+
+
+
+
+      " "#x= a correct letter of the answer
+      puts "Good job!"
+      puts "Guess again!"
+      guess = gets.chomp
+    else
+      puts "Bad news! Bunnies are multiplying! Please guess again."
+      guess = gets.chomp
+    end
