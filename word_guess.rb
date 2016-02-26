75543658d48f8d0c87fdab0bbccdaf573f467ab8
Suzanne Harrison <harrison_family@hotmail.com>
2016-02-26 13:31:17 -0800

    yay - have arrays

--- word_guess.rb
+++ word_guess.rb
@@ -27,6 +27,7 @@ YES
 
 new_game = WordGuess.new(solve_word: "carrots", number_of_lives: 5)
 letter_array = new_game.solve_word.split(//)
+all_guesses = []
 count = new_game.number_of_lives
 wrong_guess = []
 
@@ -34,27 +35,36 @@ puts welcome
 
 until count == 0
   puts "Guess a letter and save your garden!"
+  letter_array & all_guesses
+  puts "#{letter_array & all_guesses}"
   guess = gets.chomp
   if guess == letter_array[0]
   puts "#{letter_array[0]} _ _ _ _ _ _"
+  all_guesses << guess
   new_game.correct
   elsif guess == letter_array[1]
   puts "_ #{letter_array[1]} _ _ _ _ _"
+  all_guesses << guess
   new_game.correct
   elsif guess == letter_array[2]
   puts "_ _ #{letter_array[2]} #{letter_array[3]} _ _ _"
+  all_guesses << guess
   new_game.correct
   elsif guess == letter_array[4]
   puts "_ _ _ _ #{letter_array[4]} _ _"
+  all_guesses << guess
   new_game.correct
   elsif guess == letter_array[5]
   puts "_ _ _ _ _ #{letter_array[5]} _"
+  all_guesses << guess
   new_game.correct
   elsif guess == letter_array[6]
   puts "_ _ _ _ _ _ #{letter_array[6]}"
+  all_guesses << guess
   new_game.correct
   elsif guess != letter_array[0] && guess != letter_array[1] && guess != letter_array[2] && guess != letter_array[3] && guess != letter_array[4] && guess != letter_array[5] && guess != letter_array[6]
   count = count -1
+  all_guesses << guess
   wrong_guess << guess
   puts "You have already guessed: #{wrong_guess}"
   new_game.incorrect
