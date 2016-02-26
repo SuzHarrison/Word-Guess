2444de8945a9c97f49c3684eb59836b9478fd671
Jillian Boshart <Jilliliz@gmail.com>
2016-02-26 13:18:55 -0800

    One big until loop. This totally works now. We just have to figure out how to display correct guesses correctly.

--- word_guess.rb
+++ word_guess.rb
@@ -5,25 +5,12 @@ class WordGuess
       @number_of_lives = hash[:number_of_lives]
     end
 
-  #def life_loss
-  #  if @number_of_lives
-  #    @number_of_lives -= 1
-  #  elsif @number_of_lives = 0
-  #    puts "Too many bunnies! They ate your garden!"
-  #    exit
-  #  end
-  #end
-
   def correct
     puts "Yes! You got one! No bunnies in your garden!"
-    puts "Guess another letter and save your garden!"
-    guess = gets.chomp
   end
 
   def incorrect
     puts "Oh no! Another bunny is coming to your yard!"
-    puts "Guess another letter and save your garden!"
-    guess = gets.chomp
   end
 end
 
@@ -41,16 +28,13 @@ YES
 new_game = WordGuess.new(solve_word: "carrots", number_of_lives: 5)
 letter_array = new_game.solve_word.split(//)
 count = new_game.number_of_lives
+wrong_guess = []
 
 puts welcome
-guess = gets.chomp
 
-# MUST MOVE LIFE LOSS END EXIT TO UNTIL LOOP.
-# COUNT IS FINALLY WORKING! YEAAAAAAAAAAAH!
-
-#the puts statements in this loop are not currently correct. We are going to have
-# to come up with a solution for adding letters when letters have already been guessed.
 until count == 0
+  puts "Guess a letter and save your garden!"
+  guess = gets.chomp
   if guess == letter_array[0]
   puts "#{letter_array[0]} _ _ _ _ _ _"
   new_game.correct
@@ -58,10 +42,7 @@ until count == 0
   puts "_ #{letter_array[1]} _ _ _ _ _"
   new_game.correct
   elsif guess == letter_array[2]
-  puts "_ _ #{letter_array[2]} _ _ _ _"
-  new_game.correct
-  elsif guess == letter_array[3]
-  puts "_ _ _ #{letter_array[3]} _ _ _"
+  puts "_ _ #{letter_array[2]} #{letter_array[3]} _ _ _"
   new_game.correct
   elsif guess == letter_array[4]
   puts "_ _ _ _ #{letter_array[4]} _ _"
@@ -74,17 +55,11 @@ until count == 0
   new_game.correct
   elsif guess != letter_array[0] && guess != letter_array[1] && guess != letter_array[2] && guess != letter_array[3] && guess != letter_array[4] && guess != letter_array[5] && guess != letter_array[6]
   count = count -1
+  wrong_guess << guess
+  puts "You have already guessed: #{wrong_guess}"
   new_game.incorrect
   elsif count == 0
     puts "Too many bunnies! They ate your garden!"
     exit
   end
 end
-
-#      " "#x= a correct letter of the answer
-#      puts "Good job!"
-#      puts "Guess again!"
-#      guess = gets.chomp
-#    else
-#      puts "Bad news! Bunnies are multiplying! Please guess again."
-#      guess = gets.chomp
