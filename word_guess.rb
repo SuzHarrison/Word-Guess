851921e884f899f37c0b22b6abb02f6fc7b0db50
Jillian Boshart <Jilliliz@gmail.com>
2016-02-29 10:37:08 -0800

    Dovetail functionality from test file.

--- word_guess.rb
+++ word_guess.rb
@@ -14,10 +14,10 @@ class WordGuess
   end
 end
 
-# Start of program:
+#program set up
 welcome = <<YES
 
-Hello, home owner! We're going to play a game. I'm going to make you guess a
+Hello, gardner! We're going to play a game. I'm going to make you guess a
 word, and for each time you guess a letter incorrectly a new BUNNY is going to
 start to dig up your yard! HAHAHAHAHA!
 
@@ -26,35 +26,51 @@ YES
 new_game = WordGuess.new(solve_word: "ruined", number_of_lives: 5)
 letter_array = new_game.solve_word.split(//)
 all_guesses = []
+
 count = new_game.number_of_lives
 
+unguessed_answer_letters_array = []
+unguessed_answer_letters = "__" * letter_array.length
+unguessed_answer_letters_array = unguessed_answer_letters.split
+
+#begin program
 puts welcome
+puts "You have #{count} incorrect guesses before bunnies take over!"
+
 
 until count == 0
-  puts "\nGuess a letter and save your garden!"
   puts "You have already guessed: #{all_guesses}"
-  letter_array & all_guesses
-  puts "\n#{letter_array & all_guesses}"
+  puts "\nHre's your word so far: #{unguessed_answer_letters_array.join(" ")}"
+  puts "\nGuess a letter and save your garden!"
   guess = gets.chomp
-  if guess == letter_array[0]
+
+
+if guess == letter_array[0]
+  unguessed_answer_letters_array[0] = letter_array[0]
   all_guesses << guess
   new_game.correct
   elsif guess == letter_array[1]
+  unguessed_answer_letters_array[1] = letter_array[1]
   all_guesses << guess
   new_game.correct
   elsif guess == letter_array[2]
+  unguessed_answer_letters_array[2] = letter_array[2]
   all_guesses << guess
   new_game.correct
   elsif guess == letter_array[3]
+  unguessed_answer_letters_array[3] = letter_array[3]
   all_guesses << guess
   new_game.correct
   elsif guess == letter_array[4]
+  unguessed_answer_letters_array[4] = letter_array[4]
   all_guesses << guess
   new_game.correct
   elsif guess == letter_array[5]
+  unguessed_answer_letters_array[5] = letter_array[5]
   all_guesses << guess
   new_game.correct
   elsif guess == letter_array[6]
+  unguessed_answer_letters_array[6] = letter_array[6]
   all_guesses << guess
   new_game.correct
   elsif guess != letter_array[0] && guess != letter_array[1] && guess != letter_array[2] && guess != letter_array[3] && guess != letter_array[4] && guess != letter_array[5] && guess != letter_array[6]
